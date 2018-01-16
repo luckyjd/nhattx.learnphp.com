3b91edcab171727fad88d6668b8fe2aada8e2d57
luckyjd <narusunday@gmail.com>
2018-01-16 16:30:35 +0700

    update

--- pages/login.php
+++ pages/login.php
@@ -18,63 +18,59 @@
     </head>
     <body>
         <?php include 'navigation.php';?>
-        <?php
-            $secret_key = "mysecretkey";
-            $render_html = "<form action='http://simple.sso.tinhvan.com/login.php' method='POST'>
-                    <table cellpadding='0' cellspacing='0' border='1'>
-                        <tr>
-                            <td>
-                                Username :
-                            </td>
-                            <td>
-                                <input type='text' name='username' />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Password :
-                            </td>
-                            <td>
-                                <input type='password' name='password' />
-                            </td>
-                        </tr>
-                    </table>
-                    <input type='hidden' name='url_handle' value='http://nhattx.learnphp.tinhvan.com/pages/login.php'>
-                    <input type='hidden' name='secret_key' value='mysecretkey'>
-                    <input type='submit' value='Login' />
-                    <a href='../regis.php' title='Regis'>Registration</a>
-                </form>";
-            if (!empty($_SESSION["username"]) &&
-                !empty($_SESSION["token"]) &&
-                !empty($_SESSION["timeout"]) &&
-                $_SESSION["timeout"] > time()){
-                    header("Location: http://nhattx.learnphp.tinhvan.com/pages/mainpage.php");
-                    exit();
-            }
+        <form method="post" action="">
+            <table border="0" cellpadding="10" cellspacing="0">
+                <tr>
+                    <td>Username</td>
+                    <td><input type="text" id="username" name="username" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" id="password" name="password" value=""/></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='hidden' id='secret_key' value='mysecretkey'>
+                        <button type="button" onclick="loadDoc()">Login</button>
+                        <input type="reset" name="submit" value="Clear"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <div id="showerror">BBBBBB</div>
+        <script language="javascript">
+            function loadDoc() {
+                $('#showerror').html('');
+ 
+                var username = $('#username').val();
+                var password = $('#password').val();
 
-            if (isset($_POST['username']) &&
-               isset($_POST['token']) &&
-               isset($_POST['error']) &&
-               isset($_POST['expire']) &&
-               isset($_POST['secret_key']) &&
-               $_POST['secret_key'] == $secret_key){
-            
-                if ($_POST['error'] == "pass"){
-                        $_SESSION["username"] = $_POST['username'];
-                        $_SESSION["token"] = $_POST['token'];
-                        $_SESSION["timeout"] = time() + $_POST['expire'];
-                        echo $_POST['username'] . "login success";
-                        echo "Go to <a href='.mainpage.php'>Home page </a>";
-                } else {
-                    echo $_POST['error'] . '<br>';    
-                    echo $render_html;
+                // Kiểm tra dữ liệu có null hay không
+                if ($.trim(username) == ''){
+                    alert('Please enter your username.');
+                    return false;
                 }
-            } else {
-    
-                echo $render_html;
-            } 
 
-        ?>
+                if ($.trim(password) == ''){
+                    alert('Please enter your password');
+                    return false;
+                }
+                
+                str_send = "";
+                str_send += "username=" + username + "&" + "password=" + password;
+                str_send += "&" + "secret_key=" + $('#secret_key').val();
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                  if (this.readyState == 4 && this.status == 200) {
+                   document.getElementById("showerror").innerHTML = this.responseText;
+                  }
+                };
+                xhttp.open("POST", "http://simple.sso.tinhvan.com/login.php", true);
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhttp.send(str_send);
+              }
+        </script>
         <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
